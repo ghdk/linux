b7f76de0f32bd9a42aa69a7b210f93aab0947d34
Dionysios Kalofonos <peitemou@gmail.com>
2016-10-22 17:43:20 +0100

    Expands on the module for poking the module API.

---
+++ drivers/os/module.p.c
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2016 Dionysios Kalofonos
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Dionysios Kalofonos");
+MODULE_DESCRIPTION("Poke the Module API");
+
+static int __init mod_init(void)
+{
+    printk(KERN_INFO "%s: %d: %s\n", __FILE__, __LINE__, __func__);
+    return 0;
+}
+
+module_init(mod_init);
+
+static void __exit mod_exit(void)
+{
+    printk(KERN_INFO "%s: %d: %s\n", __FILE__, __LINE__, __func__);
+}
+
+module_exit(mod_exit);
+
+// eof
