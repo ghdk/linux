81d184c93dcf0811816426ccf26be0f8124788e7
Dionysios Kalofonos <peitemou@gmail.com>
2016-12-03 16:02:00 +0000

    Introduces a parameter, and cleans up the code.

--- drivers/os/modapi.c
+++ drivers/os/modapi.c
@@ -9,23 +9,20 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/moduleparam.h>
 
+#define MODULE_NAME "modapi"
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Dionysios Kalofonos");
 MODULE_DESCRIPTION("Poke the Module API");
 
-char *basename(char *n)
-{
-    char *i = NULL;
-    for(i = n; NULL != i && '\0' != *i; i++)
-        if('/' == *i)
-            n = i + 1;
-    return n;
-}
+char *device = "/dev/foo";
+module_param(device, charp, 0);
 
 static int __init mod_init(void)
 {
-    printk(KERN_INFO "%s: %d: %s\n", basename(__FILE__), __LINE__, __func__);
+    printk(KERN_INFO MODULE_NAME ": %d: %s\n", __LINE__, __func__);
+    printk(KERN_INFO MODULE_NAME ": using device: %s\n", device);
     return 0;
 }
 
@@ -33,7 +30,7 @@ module_init(mod_init);
 
 static void __exit mod_exit(void)
 {
-    printk(KERN_INFO "%s: %d: %s\n", basename(__FILE__), __LINE__, __func__);
+    printk(KERN_INFO MODULE_NAME ": %d: %s\n", __LINE__, __func__);
 }
 
 module_exit(mod_exit);
